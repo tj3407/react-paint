c8ffb7828efc3c9ef9bb7b557b374ce0009b3690
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-01-08 20:15:33 -0800

    Add paint component

---
+++ src/components/Paint/Paint.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Paint(props) {
+    React.useEffect(() => {
+
+    }, []);
+
+    return (
+        <div>
+            <canvas id="paint" />
+        </div>
+    );
+}
+
+export default Paint;
