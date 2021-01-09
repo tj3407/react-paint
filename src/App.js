f5048d4792bb1e4547cbbbcce3bd074062011b0e
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-01-09 12:38:18 -0800

    Add icons

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import React from "react";
 import { createMuiTheme } from "@material-ui/core/styles";
 import Paint from "./components/Paint/Paint";
-import './App.css';
-import { AppBar, Switch, Toolbar, Typography, ThemeProvider, CssBaseline } from "@material-ui/core";
+import "./App.css";
+import {
+  AppBar,
+  Switch,
+  Toolbar,
+  Typography,
+  ThemeProvider,
+  CssBaseline,
+  Drawer,
+  IconButton,
+  Grid,
+} from "@material-ui/core";
 import {
   orange,
   lightBlue,
   deepPurple,
-  deepOrange
+  deepOrange,
 } from "@material-ui/core/colors";
+import {
+  Create,
+  Brush,
+  LineWeight,
+  Gesture,
+  CheckBoxOutlineBlank,
+  RadioButtonChecked,
+  RadioButtonUnchecked,
+} from "@material-ui/icons";
 
 const themeObject = {
   palette: {
     type: "dark",
   },
-}
+};
 
 const light = {
   palette: {
     type: "light",
   },
-}
+};
 
 const dark = {
   palette: {
     type: "dark",
   },
-}
+};
 
 function App() {
-  const themeConfig = createMuiTheme(themeObject)
-  const [theme, setTheme] = React.useState(true)
-  const appliedTheme = createMuiTheme(theme ? light : dark)
+  const themeConfig = createMuiTheme(themeObject);
+  const [theme, setTheme] = React.useState(true);
+  const appliedTheme = createMuiTheme(theme ? light : dark);
 
   const handleThemeChange = () => {
     setTheme(!theme);
@@ -41,12 +60,43 @@ function App() {
     <ThemeProvider theme={appliedTheme}>
       <CssBaseline />
       <div className="App">
-        <AppBar position="fixed" color="inherit">
+        <AppBar position="fixed" color="inherit" elevation={0}>
           <Toolbar className="app-header">
             <Typography variant="h6">React Paint</Typography>
-            <Switch checked={theme} onChange={handleThemeChange} color="inherit" />
+            <Switch
+              checked={theme}
+              onChange={handleThemeChange}
+              color="default"
+            />
           </Toolbar>
         </AppBar>
+        <Drawer variant="permanent" PaperProps={{ className: "app-drawer" }}>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            className="drawer-content"
+          >
+            <IconButton>
+              <Create />
+            </IconButton>
+            <IconButton>
+              <Gesture />
+            </IconButton>
+            <IconButton>
+              <Brush />
+            </IconButton>
+            <IconButton>
+              <LineWeight />
+            </IconButton>
+            <IconButton>
+              <CheckBoxOutlineBlank />
+            </IconButton>
+            <IconButton>
+              <RadioButtonUnchecked />
+            </IconButton>
+          </Grid>
+        </Drawer>
         <Paint />
       </div>
     </ThemeProvider>
